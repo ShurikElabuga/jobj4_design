9c67a886e08e84d0a17b27c91435886c7ec2eb5a
Aleksandr <ShurikElabuga@yandex.ru>
2024-06-04 23:27:48 +0300

    6. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,54 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader input = new BufferedReader(
+                new FileReader(this.path))) {
+            for (String line = input.readLine(); line != null; line = input.readLine()) {
+                if (!line.isEmpty() && !line.startsWith("#")) {
+                    String[] words = line.split("=", 2);
+                    if (words.length != 2 || words[0].isBlank() || words[1].isBlank()) {
+                        throw new IllegalArgumentException("wrong template");
+                    }
+                    values.put(words[0], words[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner output = new StringJoiner(System.lineSeparator());
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.path))) {
+            reader.lines().forEach(output::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return output.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("data/app.properties"));
+
+    }
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ConfigTest {
+    @Test
+    void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("cadet2")).isEqualTo("Guliev");
+        assertThat(config.value("cadet3")).isEqualTo("Samsonov=");
+    }
+
+    @Test
+    void whenPairWithoutCommentIsException() {
+        String path = "./data/lines_contain_wrong_template.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("wrong template");
+    }
+
+    @Test
+    void whenPairWithComment() {
+        String path = "./data/pait_with_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("cadet4")).isEqualTo("Sidorov");
+    }
+}

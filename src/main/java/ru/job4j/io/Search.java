99995b9f56eb6acfe71f6a8819508941badc8c50
Aleksandr <ShurikElabuga@yandex.ru>
2024-06-26 23:16:22 +0300

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,7 +1,7 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
@@ -9,9 +9,26 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName()
-                .endsWith(".js")).forEach(System.out::println);
+        File start = new File(args[0]);
+        String str = args[1];
+        checkArgs(args, start, str);
+        search(start.toPath(), path -> path.toFile().getName()
+                .endsWith(str)).forEach(System.out::println);
+    }
+
+    public static void checkArgs(String[] args, File file, String string) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+        }
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        if (string.isEmpty()) {
+            throw new IllegalArgumentException("No search data");
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {

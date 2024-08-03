66f6f8fcdd9e8d21ee52e25464fbe85043c7f654
Aleksandr <ShurikElabuga@yandex.ru>
2024-08-03 22:59:24 +0300

    0. Что такое база данных. [#6860]

---
+++ db/students.sql
@@ -0,0 +1,10 @@
+create table studets(
+	name varchar(255),
+	data text,
+	course int
+)
+insert into students(name, date, course) values('Boris Borisov', '04.04.1994', 5);
+
+update students set name = 'Vladimir Vladimirov', date = '12.12.1993';
+
+delete from students;

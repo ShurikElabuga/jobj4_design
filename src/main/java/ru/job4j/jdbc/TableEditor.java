c67c1c3f86d0519d4a0553beff9b6813376ed4e8
Aleksandr <ShurikElabuga@yandex.ru>
2024-09-04 17:10:25 +0300

    0.1. Statement [#379306]

---
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -0,0 +1,105 @@
+package ru.job4j.jdbc;
+
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+import java.util.StringJoiner;
+
+public class TableEditor implements AutoCloseable {
+    private Connection connection;
+
+    private Properties properties;
+
+    public TableEditor(Properties properties) {
+        this.properties = properties;
+        initConnection();
+    }
+
+    private void initConnection() {
+        connection = null;
+    }
+
+    private void runScript(String sql, String tableName) {
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+            System.out.println(getTableScheme(tableName));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void createTable(String tableName) {
+        String sql = String.format("create table if not exists %s(%s, %s);",
+                tableName, "id serial primary key", "name text");
+        runScript(sql, tableName);
+    }
+
+    public void dropTable(String tableName) {
+        String sql = String.format("drop table if exists %s;", tableName);
+        runScript(sql, tableName);
+    }
+
+    public void addColumn(String tableName, String columnName, String type) {
+        String sql = String.format("alter table %s add column %s %s;",
+                tableName, columnName, type);
+        runScript(sql, tableName);
+    }
+
+    public void dropColumn(String tableName, String columnName) {
+        String sql = String.format("alter table %s drop column %s;",
+                tableName, columnName);
+        runScript(sql, tableName);
+    }
+
+    public void renameColumn(String tableName, String columnName, String newColumnName) {
+        String sql = String.format("alter table %s rename column %s to %s;",
+                tableName, columnName, newColumnName);
+        runScript(sql, tableName);
+    }
+
+
+    public String getTableScheme(String tableName) throws Exception {
+        var rowSeparator = "-".repeat(30).concat(System.lineSeparator());
+        var header = String.format("%-15s|%-15s%n", "NAME", "TYPE");
+        var buffer = new StringJoiner(rowSeparator, rowSeparator, rowSeparator);
+        buffer.add(header);
+        try (var statement = connection.createStatement()) {
+            var selection = statement.executeQuery(String.format(
+                    "SELECT * FROM %s LIMIT 1", tableName
+            ));
+            var metaData = selection.getMetaData();
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                buffer.add(String.format("%-15s|%-15s%n",
+                        metaData.getColumnName(i), metaData.getColumnTypeName(i))
+                );
+            }
+        }
+        return buffer.toString();
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Properties config = new Properties();
+        try (InputStream in = TableEditor.class.getClassLoader()
+                .getResourceAsStream("app.properties")) {
+            config.load(in);
+        }
+        String url = config.getProperty("url");
+        String login = config.getProperty("username");
+        String password = config.getProperty("password");
+        try (var tableEditor = new TableEditor(config)) {
+            tableEditor.connection = DriverManager.getConnection(url, login, password);
+            tableEditor.createTable("created_table");
+            tableEditor.addColumn("created_table", "created_column", "text");
+            tableEditor.renameColumn("created_table", "created_column", "renamed_column");
+            tableEditor.dropColumn("created_table", "renamed_column");
+            tableEditor.dropTable("created_table");
+        }
+    }
+}

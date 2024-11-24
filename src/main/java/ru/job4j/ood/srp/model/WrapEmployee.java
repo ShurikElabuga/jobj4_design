89b1143b40f35a526322e696f64f1debca631d12
ShurikElabuga <ShurikElabuga@yandex.ru>
2024-11-24 19:57:06 +0300

    1. Поддержка форматов XML, JSON в генераторе отчетов. [#851 #523328]

--- src/main/java/ru/job4j/ood/srp/model/WrapEmployee.java
+++ src/main/java/ru/job4j/ood/srp/model/WrapEmployee.java
@@ -60,8 +60,12 @@ public class WrapEmployee {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         WrapEmployee that = (WrapEmployee) o;
         return Objects.equals(name, that.name);
     }

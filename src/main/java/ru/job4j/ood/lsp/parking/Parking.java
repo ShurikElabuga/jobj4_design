c0f094bf497a4b7f0411d31b4ca5dd9255dbecaf
ShurikElabuga <ShurikElabuga@yandex.ru>
2025-01-09 21:18:41 +0300

    2. Парковка машин [#853 #527658] реализация кода

--- src/main/java/ru/job4j/ood/lsp/parking/Parking.java
+++ src/main/java/ru/job4j/ood/lsp/parking/Parking.java
@@ -7,7 +7,7 @@ public class Parking implements Park {
 
     private int placeCar;
     private int placeTruck;
-    private List<Vehicle> vehicles = new ArrayList<>();
+    private List<Vehicle> listVehicles = new ArrayList<>();
 
     public Parking(int placeCar, int placeTruck) {
         this.placeCar = placeCar;
@@ -16,6 +16,21 @@ public class Parking implements Park {
 
     @Override
     public boolean parking(Vehicle vehicle) {
-        return false;
+        boolean result = false;
+        int size = vehicle.getSize();
+        if (size == 1 && placeCar > 0) {
+            listVehicles.add(vehicle);
+            placeCar--;
+            result = true;
+        } if (size > 1 && placeTruck > 0) {
+            listVehicles.add(vehicle);
+            placeTruck--;
+            result = true;
+        } if (size > 1 && size <= placeCar) {
+            listVehicles.add(vehicle);
+            placeCar--;
+            result = true;
+        }
+        return result;
     }
 }

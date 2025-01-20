a024469e8bbc7c556ce0b6f99af7ea2dab70adba
ShurikElabuga <ShurikElabuga@yandex.ru>
2025-01-20 22:21:09 +0300

    Task173 is changed

--- src/main/java/ru/job4j/tasks/Task173.java
+++ src/main/java/ru/job4j/tasks/Task173.java
@@ -3,16 +3,20 @@ package ru.job4j.tasks;
 public class Task173 {
 
     public static String code(String s) {
+
         if (s.length() == 0) {
             return "empty";
         }
         if (s.length() > 0 && s.length() <= 4) {
             return s;
         }
+
         char[] str = s.toCharArray();
+
         for (int i = 0; i < str.length - 4; i++) {
             str[i] = '#';
         }
+
         return String.valueOf(str);
     }
 

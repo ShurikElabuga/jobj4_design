94bb7d7464e2a0ca09b8fe27039df661dd513b54
ShurikElabuga <ShurikElabuga@yandex.ru>
2025-02-15 22:30:56 +0300

    Сортировка слиянием [#504997 #532752]

--- src/main/java/ru/job4j/algo/sort/Merge.java
+++ src/main/java/ru/job4j/algo/sort/Merge.java
@@ -18,7 +18,6 @@ public class Merge {
     private static int[] merge(int[] left, int[] right) {
        int[] result = new int[left.length + right.length];
        int i = 0, j = 0, k = 0;
-
        while (i < left.length && j < right.length) {
            if (left[i] <= right[j]) {
                result[k++] = left[i++];
@@ -26,11 +25,9 @@ public class Merge {
                result[k++] = right[j++];
            }
        }
-
        while (i < left.length) {
            result[k++] = left[i++];
        }
-
        while (j < right.length) {
            result[k++] = right[j++];
        }
